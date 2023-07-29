665d754da5b8714d2bc1788e026fd6fba7aa0505
ednatrejo <Trejo.edna1379@gmail.com>
2023-07-29 15:29:40 -0500

    update modal closed

--- scripts/index.js
+++ scripts/index.js
@@ -30,5 +30,13 @@ const profileEditModal = document.querySelector("#profile-edit-modal");
 
 
 profileEditButton.addEventListener('click', () => {
-  profileEditModal.classList.add('modal_opened');
+  profileEditModal.classList.add('modal__opened');
+})
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+
+profileEditButton.addEventListener('click', () => {
+  profileEditModal.classList.remove('modal__closed');
 })
