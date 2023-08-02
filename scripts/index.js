67e8cc7d865f8ecb83b8770db6e8679925de1170
ednatrejo <Trejo.edna1379@gmail.com>
2023-08-01 21:21:49 -0500

    update js

--- scripts/index.js
+++ scripts/index.js
@@ -24,7 +24,7 @@ const initialCards = [
   link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg ",
 },
 ];
-
+``
 /* --------------------------------------------------------------------------------*/
 /*                              Elements                                          */
 /* --------------------------------------------------------------------------------*/
@@ -37,6 +37,8 @@ const profileDescription = document.querySelector('.profile__description');
 const profileTitleInput = document.querySelector("#profile-title-input");
 const profileDescriptionInput = document.querySelector("#profile-discription-input");
 const profileEditForm = profileEditModal.querySelector(".modal__form");
+con
+const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
 
 /* --------------------------------------------------------------------------------*/
 /*                              Functions                                         */
@@ -45,7 +47,24 @@ const profileEditForm = profileEditModal.querySelector(".modal__form");
 function closePopop() {
   profileEditModal.classList.remove("modal__opened");
 }
+function getCardElment(cardData) {
+    // clone the template element with all its content and store it in a cardElement variable
+    const cardElement = cardTemplate.cloneNote(true);
+    //access the card title and image and store them in variables
+   const cardImageEl = cardElement.querySelector(".card__image");
+   const cardTitleEl = cardElement.querySelector(".card__title");
+   //set the path to the image to the link field of the object
+   // set the image alt text to the name field of the object
+   cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.name;
+  cardTitleEl.textContent = cardData.name;
+   // set the card title to the name field of the object, too
+   cardTitleEl.textContent = cardData.name;
+    //return the ready HTML element with the filled-in data
+    //return cardElement;
+    return cardElement;
 
+}
 /* --------------------------------------------------------------------------------*/
 /*                              Event Handlers                                     */
 /* --------------------------------------------------------------------------------*/
@@ -71,3 +90,8 @@ profileEditButton.addEventListener('click', () => {
 profileEditCloseButton.addEventListener("click", closePopop);
 
 profileEditForm.addEventListener("sumbit", handleProfileEditSumbit);
+
+initialCards.forEach((cardData)=> {
+const cardElement = getCardElement(cardData);
+  cardListEl.prepend(cardElement);
+});
