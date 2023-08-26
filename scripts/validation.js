3b8d872be5e54701aba02bfdf775d5cd6766ef8b
ednatrejo <Trejo.edna1379@gmail.com>
2023-08-26 12:30:10 -0500

    sprint 6

--- scripts/validation.js
+++ scripts/validation.js
@@ -9,40 +9,45 @@ const config = {
   errorClass: "modal__error_visible",
 };
 
-
-
 // Error message //
-function showInputError(formElements,inputElements, options { inputErrorClass, errorClass}) {
-inputElements.classList.add(inputErrorClass);
-const errorMessageElements = formElements.querySelector('#${inputElements.id}-error');
-errorMessageElements.textContent = inputElements.validationMessage;
-errorMessageElements.classList.add(errorClass);
+function showInputError(
+  formElements,
+  inputElements,
+  options,
+  { inputErrorClass, errorClass }
+) {
+  inputElements.classList.add(inputErrorClass);
+  const errorMessageElements = formElements.querySelector(
+    "#${inputElements.id}-error"
+  );
+  errorMessageElements.textContent = inputElements.validationMessage;
+  errorMessageElements.classList.add(errorClass);
 }
 
-function hideInputError(formElements,inputElements, options { inputErrorClass, errorClass}) {
+function hideInputError(
+  formElements,
+  inputElements,
+  options,
+  { inputErrorClass, errorClass }
+) {
   inputElements.classList.remove(inputErrorClass);
-  const errorMessageElements = formElements.querySelector('#${inputElements.id}-error');
+  const errorMessageElements = formElements.querySelector(
+    "#${inputElements.id}-error"
+  );
   errorMessageElements.textContent = "";
   errorMessageElements.classList.remove(errorClass);
-  }
-
-
-
-
+}
 
 //enabling validation by calling enableValidation()
 // pass all the settings on call
 // validity checkers and input //
 function checkInputValidity(formElements, inputElements, options) {
-if(!inputElements.validity.valid) {
-  showInputError(formElements, inputElements, options);
-} else {
-  hideInputError(formElements,inputElements, options);
+  if (!inputElements.validity.valid) {
+    showInputError(formElements, inputElements, options);
+  } else {
+    hideInputError(formElements, inputElements, options);
+  }
 }
-}
-
-
-
 
 // Button state //
 const disableButton = (submitButton, { inactiveButtonClass }) => {
@@ -55,52 +60,52 @@ const enableButton = (submitButton, { inactiveButtonClass }) => {
   submitButton.disabled = false;
 };
 
+function toggleButtonState(
+  inputElements,
+  submitButton,
+  { inactiveButtonClass }
+) {
+  let foundInvalid = false;
 
+  inputElements.forEach((inputElements) => {
+    if (!inputElements.validity.valid) {
+    }
+  });
 
-function toggleButtonState(inputElements, submitButton{inactiveButtonClass}) {
-let foundInvalid = false;
-
-inputElements.forEach(inputElements =>{
-if(!inputElements.validity.valid) {
-
+  if (foundInvalid) {
+    submitButton.classList.add(inactiveButtonClass);
+    submitButton.disabled = true;
+  } else {
+    submitButton.classList.remove(inactiveButtonClass);
+    submitButton.disabled = false;
+  }
+  enableButton(submitButton, { inactiveButtonClass });
 }
-});
-
-if(foundInvalid) {
-  submitButton.classList.add(inactiveButtonClass);
-submitButton.disabled = true;
-} else {
-  submitButton.classList.remove(inactiveButtonClass);
-  submitButton.disabled = false;
-}
-enableButton(submitButton, { inactiveButtonClass });
-};
-
-
 
 // Listeners and validation //
 function setEventListeners(formElements, options) {
-  const { inputSelector} = options;
-  const inputElements = [...formElement.querySelectorAll(inputSelector)];
-  const submitButton = formElement.querySelector(".modal__button");
+  const { inputSelector } = options;
+  const inputElements = [...formElements.querySelectorAll(inputSelector)];
+  const submitButton = formElements.querySelector(".modal__button");
 
-  inputElements.forEach((inputElement) => {
-    inputElement.addEventListener("input", (e) => {
+  inputElements.forEach((inputElements) => {
+    inputElements.addEventListener("input", (e) => {
       checkInputValidity(formElements, inputElements, options);
-      toggleButtonState(inputElements, submitButton, options;
+      toggleButtonState(inputElements, submitButton, options);
     });
   });
 
   toggleButtonState(inputElements, submitButton, options);
 }
 
-function enableValidation(options); {
-const formElements = [...document.querySelectorAll(options.formSelector)];
-formElements.forEach("sumbit", (e) => {
-e.preventDefault();
-});
-setEventListeners(formElements, options);
-
+function enableValidation(options) {
+  const formElements = [...document.querySelectorAll(options.formSelector)];
+  formElements.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElements, options);
+  });
 }
 
 enableValidation(config);
