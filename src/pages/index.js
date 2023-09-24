aa0ad92a1e7afea65e683e74410ff978902972d0
ednatrejo <Trejo.edna1379@gmail.com>
2023-09-23 23:42:12 -0500

    update error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
-import "../index.js";
+import "../pages/index.css";
 import Section from "../components/Section.js";
 import Popup from "../components/Popup.js";
 
