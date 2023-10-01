cc7ef456bba23b22a343e6d9663521e0995b1c4b
ednatrejo <Trejo.edna1379@gmail.com>
2023-10-01 14:04:23 -0500

    update errors

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,24 +1,25 @@
 export default class Popup {
   constructor({ popupSelector }) {
     this._popupElement = document.querySelector(popupSelector);
-    this._handleEscClose = this._handleEscClose.bind(this);
+    this._popupCloseBtn = this._popupElement.querySelector(".modal__close");
+    this._popupImage = document.querySelector(".preview-image");
   }
 
   open() {
     this._popupElement.classList.add("modal_opened");
-    document.addEventListener("keyup", this._handleEscClose);
+    document.addEventListener("keydown", this._handleEscClose);
   }
 
   close() {
     this._popupElement.classList.remove("modal_opened");
-    document.removeEventListener("keyup", this._handleEscClose);
+    document.removeEventListener("keydown", this._handleEscClose);
   }
 
-  _handleEscClose(evt) {
-    if (evt.which === ESC_KEYCODE) {
+  _handleEscClose = (evt) => {
+    if (evt.key === "Escape") {
       this.close();
     }
-  }
+  };
 
   setEventListeners() {
     this._popupElement.addEventListener("click", (evt) => {
@@ -29,12 +30,8 @@ export default class Popup {
         this.close();
       }
     });
-    D;
     this._popupCloseBtn.addEventListener("click", () => {
       this.close();
-    });
-    this._popupImage.addEventListener("click", () => {
-      this.open();
     });
   }
 }
